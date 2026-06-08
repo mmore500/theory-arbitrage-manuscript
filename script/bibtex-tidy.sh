4eba4c210260a3bca48135b02b2fd0b1992d4805
Matthew Andres Moreno <mmore500.login@gmail.com>
2026-06-08 17:06:25 -0400

    Update bibtex-tidy.sh from paperproject

--- script/bibtex-tidy.sh
+++ script/bibtex-tidy.sh
@@ -8,4 +8,4 @@ cd "${script_dir}"
 cd ..
 
 cp bibl.bib /tmp/bibl.bib
-bibtex-tidy --modify --omit=abstract,keywords --curly --numeric --space=2 --align=0 --sort=key --duplicates=key,doi --merge=combine --strip-enclosing-braces --drop-all-caps --no-escape --sort-fields=title,shorttitle,author,year,month,day,journal,booktitle,location,on,publisher,address,series,volume,number,pages,doi,isbn,issn,url,urldate,copyright,category,note,metadata --trailing-commas --remove-empty-fields --no-backup bibl.bib
+bibtex-tidy --omit=abstract,keywords --curly --numeric --space=2 --align=0 --sort=key --duplicates=key,doi --merge=combine --strip-enclosing-braces --drop-all-caps --no-escape --sort-fields=title,shorttitle,author,year,month,day,journal,booktitle,location,on,publisher,address,series,volume,number,pages,doi,isbn,issn,url,urldate,copyright,category,note,metadata --trailing-commas --remove-empty-fields --no-backup bibl.bib
